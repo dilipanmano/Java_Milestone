720c8b5617afaa877992dc6fa4dededa9c37fe2e
Dilipan M <40638312+dilipanmano@users.noreply.github.com>
2018-08-20 21:16:27 +0530

    Create five.java

---
+++ five.java
@@ -0,0 +1,7 @@
+public class five {
+public static void main(String args[]) {
+double sum=375,ind,ina=3,inb=4,inc=5,deca=4,decb=5,decc=6;
+ind=(sum-(deca+decb+decc))/12;
+System.out.printf("Salary of A=%f\nSalary of B=%f\nSalary of C=%f",(ina*ind)+deca,(inb*ind)+decb,(inc*ind)+decc);
+}
+}
