53b705a59186076a9b5b573d5368ae1ce6852bd1
Dilipan M <40638312+dilipanmano@users.noreply.github.com>
2018-08-20 21:23:56 +0530

    Create six.java

---
+++ six.java
@@ -0,0 +1,8 @@
+public class six {
+	public static void main(String[] args) {
+float tot=50,secnd=90,trd=2/3,pass,attend;
+attend=tot*(90/100)
+pass=(attend*2)/3;
+System.out.printf("No. of students passed=%f",pass);
+}
+}
